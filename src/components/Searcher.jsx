4cbd3fec2172046a1d67bf0b7142ed09df4cdfe3
yfove <curtis.ch.chat@gmail.com>
2025-02-06 17:34:37 -0700

    refactor main archive components

--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -31,12 +31,12 @@ function Search({ searchList }) {
     <div class="">
       <label
         htmlFor="search"
-        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        className="sr-only mb-2 font-medium text-gray-900 text-sm dark:text-white"
       >
         Search
       </label>
       <div className="relative ">
-        <div className="absolute m-3 inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <div className="pointer-events-none absolute inset-y-0 left-0 m-3 flex items-center pl-3">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="icon icon-tabler icon-tabler-search"
@@ -44,7 +44,7 @@ function Search({ searchList }) {
             height={24}
             viewBox="0 0 24 24"
             strokeWidth="2"
-            stroke="darkgray"
+            stroke="black"
             fill="none"
             strokeLinecap="round"
             strokeLinejoin="round"
@@ -55,22 +55,22 @@ function Search({ searchList }) {
           </svg>
         </div>
         <div class="max-md:flex max-md:justify-center">
-        <input
-          type="text"
-          id="search"
-          value={query}
-          onChange={handleOnSearch}
-          class="block lg:w-3/4 p-2 m-4 pl-10 text-sm 
-                text-gray-400 
-                border border-gray-800
-                rounded-lg bg-black
+          <input
+            type="text"
+            id="search"
+            value={query}
+            onChange={handleOnSearch}
+            class="m-4 block rounded-lg border-2 border-gray-800 p-2 
+                pl-10 
+                text-gray-400 text-sm
+                focus:border-gray-700 
                 focus:outline-none
                 focus:ring-gray-700
-                focus:border-gray-700
-                max-sm:w-full"
-          placeholder="Search for anything..."
-        />
-</div>
+                max-sm:w-full
+                lg:w-3/4"
+            placeholder="Search for anything..."
+          />
+        </div>
       </div>
 
       {query.length > 1 && (
@@ -80,12 +80,19 @@ function Search({ searchList }) {
         </div>
       )}
 
-      <ul className="list-none m-4">
+      <ul className="m-4 list-none">
         {posts &&
           posts.map((post) => (
             <li className="py-2">
-              <a className="text-lg text-indigo-400 hover:text-indigo-500 hover:underline underline-offset-2" href={`/post/${post.frontmatter.slug}`}>{post.frontmatter.title}</a>
-              <p className="text-sm text-gray-400">{post.frontmatter.description}</p>
+              <a
+                className="text-black underline-offset-2 text-lg hover:text-amber-500 hover:underline"
+                href={`/post/${post.frontmatter.slug}`}
+              >
+                {post.frontmatter.title}
+              </a>
+              <p className="text-gray-400 text-sm">
+                {post.frontmatter.description}
+              </p>
             </li>
           ))}
       </ul>
@@ -94,4 +101,3 @@ function Search({ searchList }) {
 }
 
 export default Search;
-
